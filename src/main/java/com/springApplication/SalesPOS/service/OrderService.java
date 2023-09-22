ee93d04e318ad6a19327cae200a8ae23dd5cfcf0
Pasindu Lakmal <nanayakk-cs18027@stu.kln.ac.lk>
2023-09-23 00:27:44 +0530

    SalesPOS

---
+++ src/main/java/com/springApplication/SalesPOS/service/OrderService.java
@@ -0,0 +1,4 @@
+package com.springApplication.SalesPOS.service;
+
+public interface OrderService {
+}
---
+++ src/main/java/com/springApplication/SalesPOS/service/impl/OrderServiceIMPL.java
@@ -0,0 +1,8 @@
+package com.springApplication.SalesPOS.service.impl;
+
+import com.springApplication.SalesPOS.service.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceIMPL implements OrderService {
+}
